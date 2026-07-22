58385c4b4b5598e9acefc894452f98b34177a02a
Jatin Sangwan <jatinsangwan@Jatins-Laptop.local>
2026-07-23 01:08:53 +0530

    this is one of the ways to do it...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from turtle import title
+from fastapi import Body, FastAPI
+
 
 app = FastAPI()
 
@@ -6,3 +8,25 @@ app = FastAPI()
 @app.get("/")  # this decorator is what converts this function here into a route or in FASTAPI we say path operation
 def get():    # this is a python func and here the async keyword is operation only needed if the work is asynchronous
     return {"message": "hello world and we are in disect branch with a NOTES.txt"}
+
+
+@app.get("/posts")
+def get_posts():
+    # logic here
+
+    return {"data": "here the posts will come..."}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    print(type(payload))
+    data = {}
+    for key in payload:
+        print(f"This is the {key}: {payload[key]}")
+        data[key] = payload[key]
+    return {
+        "message": "we received the data and the post is created",
+        "title": data["title"],
+        "content": data["content"]
+        }
