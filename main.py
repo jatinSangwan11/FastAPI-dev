26cb6c2880f4ec9a782224631678c1e5d8eaaa3a
Jatin Sangwan <jatinsangwan@Jatins-Laptop.local>
2026-07-23 00:26:33 +0530

    small disection and added the NOTES.txt

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from email import message
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {message: "hello world"}
+#------- this is the path or the url --------
+@app.get("/")  # this decorator is what converts this function here into a route or in FASTAPI we say path operation
+def get():    # this is a python func and here the async keyword is operation only needed if the work is asynchronous
+    return {"message": "hello world and we are in disect branch with a NOTES.txt"}
